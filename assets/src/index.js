fb6d7c54d9dcb42a4ae5dbda9ee860ee9cfe7324
David Mihal <david@dmihal.com>
2019-06-07 16:53:56 +0300

    Export abstract assets

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,4 +1,7 @@
+module.exports.Asset = require('./Asset');
+module.exports.ERC20Asset = require('./ERC20Asset');
+module.exports.NativeAsset = require('./NativeAsset');
+
 module.exports.dai = require('./dai');
 module.exports.eth = require('./eth');
 module.exports.xdai = require('./xdai');
-
