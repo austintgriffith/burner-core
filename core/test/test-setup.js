5c64f8594e48eb426be73744c7b8c0f9b0b05689
David Mihal <david@dmihal.com>
2019-06-14 12:25:24 +0100

    Log stack if tests do not exit gracefuly

--- core/test/test-setup.js
+++ core/test/test-setup.js
@@ -1,5 +1,12 @@
+const log = require('why-is-node-running')
 const { eth } = require('@burner-wallet/assets');
 
 after(() => {
   eth.stop();
+
+  setTimeout(() => {
+    console.error('Tests have not exited gracefully');
+    log();
+    process.exit(1);
+  }, 10000).unref();
 });
