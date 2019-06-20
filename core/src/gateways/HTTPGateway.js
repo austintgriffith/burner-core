0fe67065299937d89f43f53403791c1343cf8835
David Mihal <david@dmihal.com>
2019-06-21 00:30:28 +0100

    Add HTTPGateway

---
+++ core/src/gateways/HTTPGateway.js
@@ -0,0 +1,37 @@
+const Gateway = require('./Gateway');
+const Web3 = require('web3');
+
+class HTTPGateway extends Gateway {
+  constructor(rpcUrl, networkId) {
+    super();
+    this.rpcUrl = rpcUrl;
+    this.networkId = networkId;
+    this._w3Provider = null;
+  }
+
+  isAvailable() {
+    return true;
+  }
+
+  getNetworks() {
+    return [this.networkId];
+  }
+
+  _provider(network) {
+    if (!this._w3Provider) {
+      this._w3Provider = new Web3.providers.HttpProvider(this.rpcUrl);
+    }
+    return this._w3Provider;
+  }
+
+  async send(network, { method, params, id }) {
+    if (network !== thsi.networkId) {
+      throw new Error('HTTPGateway does not support this network');
+    }
+    const response = await this._provider(network).send(method, params);
+
+    return response;
+  }
+}
+
+module.exports = HTTPGateway;
--- core/src/gateways/index.js
+++ core/src/gateways/index.js
@@ -1,3 +1,4 @@
+module.exports.HTTPGateway = require('./HTTPGateway');
 module.exports.InfuraGateway = require('./InfuraGateway');
 module.exports.InjectedGateway = require('./InjectedGateway');
 module.exports.XDaiGateway = require('./XDaiGateway');
