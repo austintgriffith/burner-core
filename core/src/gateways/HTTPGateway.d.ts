c657d2a7bf4a2e1e4ff055b09bf6f95aade98bb3
David Mihal <david@dmihal.com>
2019-07-29 20:14:47 +0300

    Create ts def for HTTPGateway

---
+++ core/src/gateways/HTTPGateway.d.ts
@@ -0,0 +1,5 @@
+import Gateway from './Gateway';
+
+export default class HTTPGateway extends Gateway {
+  constructor(rpcUrl: string, networkId: string);
+}
--- core/src/gateways/index.d.ts
+++ core/src/gateways/index.d.ts
@@ -1,5 +1,5 @@
 export { default as Gateway } from './Gateway';
-export { default as HTTPGateway } from './Gateway';
+export { default as HTTPGateway } from './HTTPGateway';
 export { default as InfuraGateway } from './InfuraGateway';
 export { default as InjectedGateway } from './Gateway';
 export { default as XDaiGateway } from './Gateway';
