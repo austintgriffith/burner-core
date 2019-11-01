c2160058fb60bbf3d80779a2a68ebd53e66baeab
David Mihal <david@dmihal.com>
2019-11-01 14:32:27 -0700

    Return Web3 type

--- core/src/BurnerCore.d.ts
+++ core/src/BurnerCore.d.ts
@@ -1,4 +1,5 @@
 import { Asset } from '@burner-wallet/assets';
+import Web3 from 'web3';
 import { Gateway } from './gateways';
 import { Signer } from './signers';
 import HistoryEvent, { HistoryEventProps } from './HistoryEvent';
@@ -19,7 +20,7 @@ export default class BurnerCore {
   shouldSkipSigning(network:string, txParams:any): boolean;
   handleRequest(network: string, payload: any): void;
   getProvider(network: string): any;
-  getWeb3(network: string, options?: any): any;
+  getWeb3(network: string, options?: any): Web3;
   canCallSigner(action: string, account: string): boolean;
   callSigner(action: string, account: string, ...params: any[]): any;
 
